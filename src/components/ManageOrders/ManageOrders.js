6ab9e85a5f4cfeab56b13cfdfc878704453018d6
ghost <imransardar122@gmail.com>
2021-10-31 10:30:46 +0600

    manage order

--- src/components/ManageOrders/ManageOrders.js
+++ src/components/ManageOrders/ManageOrders.js
@@ -1,9 +1,95 @@
 import React from 'react';
+import { Button, Table } from 'react-bootstrap';
+import './ManageOrder.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
+import useOrder from '../../hooks/useOrder';
+import useAuth from '../../hooks/useAuth';
+
 
 const ManageOrders = () => {
+    const element = <FontAwesomeIcon icon={faTrash} />
+
+
+    const [order, setOrder] = useOrder();
+
+    //DELETE an order from list
+    const handelDeleteOrder = id => {
+        const proceed = window.confirm('Are you sure you want to delete?')
+        if (proceed) {
+            const url = `http://localhost:5000/orderlist/${id}`;
+            fetch(url, {
+                method: 'DELETE',
+            })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert('deleted successfully')
+                        const remainingOrder = order.filter(restOrder => restOrder._id !== id);
+                        setOrder(remainingOrder)
+                    }
+                })
+        }
+    }
+    //confirmation status change method
+
     return (
-        <div>
-            <h2>manage orders</h2>
+        <div className="table-data">
+            <Table striped bordered hover variant="dark">
+                <thead>
+                    <tr>
+                        <th>No</th>
+                        <th>Buyer Name</th>
+                        <th>Service Name</th>
+                        <th>Price</th>
+                        <th>Order Status</th>
+                        <th>Order Update</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        order.map(final =>
+
+                            <>
+
+                                <tr>
+
+                                    <td>1</td>
+                                    <td>{final.name}</td>
+                                    <td>{final.servicename}</td>
+                                    <td>{final.price}</td>
+                                    {final.status === false ?
+                                        <>
+                                            <td>
+                                                Pending
+                                                <Button className="confirm-button">Approve</Button>
+                                            </td>
+
+
+                                        </>
+
+
+                                        :
+                                        <td>Approved</td>
+                                    }
+
+
+                                    <td>
+                                        <Button onClick={() => handelDeleteOrder(final._id)} data-hover="Delete" className="delete-button" variant="danger">{element}</Button>
+
+
+
+                                    </td>
+                                </tr>
+                            </>
+
+                        )
+                    }
+
+
+
+                </tbody>
+            </Table>
         </div>
     );
 };
